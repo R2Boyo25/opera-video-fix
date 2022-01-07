2d4c813c0312782ecbf2c7c9693d4de1c62536a5
R2boyo25 <r2boyo25@gmail.com>
2022-01-07 17:03:53 -0600

    oops... backticks run commands

--- opera-ffmpeg-fix.sh
+++ opera-ffmpeg-fix.sh
@@ -1,4 +1,4 @@
-echo "This may break opera, run `apm uninstall r2boyo25/opera-video-fix` if opera doesn't start after you try to run it."
+echo "This may break opera, run 'apm uninstall r2boyo25/opera-video-fix' if opera doesn't start after you try to run it."
 
 wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-codecs-ffmpeg_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb
 ar x chromium-codecs-ffmpeg_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb data.tar.xz
