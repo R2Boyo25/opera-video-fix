ab24f7351c6e71f738abe40c5fbbd32d9b1e9a8c
R2boyo25 <r2boyo25@gmail.com>
2022-01-07 17:01:28 -0600

    warning

--- opera-ffmpeg-fix.sh
+++ opera-ffmpeg-fix.sh
@@ -1,3 +1,5 @@
+echo "This may break opera, run `apm uninstall r2boyo25/opera-video-fix` if opera doesn't start after you try to run it."
+
 wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-codecs-ffmpeg_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb
 ar x chromium-codecs-ffmpeg_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb data.tar.xz
 tar -xJf data.tar.xz --strip-components=4 ./usr/lib/chromium-browser/libffmpeg.so
